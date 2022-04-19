dc2ce5c6b86a0cd3e9aa19c4124cf9ff7a1dfac3
himlayyy <alesandralelis@gmail.com>
2022-04-20 00:05:14 +0800

    Can now receive the clicked todo from Todo component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Boards from "./components/Boards";
+import Todo from "./components/Todo";
+import Details from "./components/Details";
+import { TodoProvider } from "./components/TodoContext";
+import { useContext, useState } from 'react';
+import { TodoContext} from "./components/TodoContext"
 
 function App() {
+  const [todoHandler, setTodoHandler] = useState({
+    title: "",
+    date: ""
+  });
+
+  const handleClickedItem = (clicked) =>{
+    console.log(typeof(clicked), clicked);
+    console.log(typeof(todoHandler));
+    setTodoHandler(    
+      { 
+        title: clicked.title,
+        date: clicked.date
+      }
+      );
+      // console.log(typeof )
+    console.log(clicked.title);
+    // console.log(todoHandler);
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav id="navbar">Todo App</nav>
+      <div className="workspace">
+        <div className="boards">
+          <div className="container">
+            <div className="container-header">Boards</div> <Boards />
+          </div>
+        </div>
+        <TodoProvider>
+          <div className="todo">
+            <div className="container">
+              <div className="container-header">Todo</div>
+              <Todo clickedHandler={handleClickedItem}/>
+              {/* <Todo/> */}
+
+            </div>
+          </div>
+          <div className="details">
+            <div className="container">
+              {/* <div className="container-header">Details title={title} date={date}</div> */}
+              <div className="container-header">Details </div>
+              {/* <Details/> */}
+              
+              <Details item={todoHandler}/>
+            </div>
+          </div>
+        </TodoProvider>
+      </div>
+    </>
   );
 }
 
