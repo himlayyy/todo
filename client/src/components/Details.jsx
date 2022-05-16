a940186c97f6b926edce7dcc1b6b536129b56f3e
himlayyy <alesandralelis@gmail.com>
2022-05-16 13:00:53 +0800

    Details can now fetch the edited details. Issue: Need to click on other Todo to load the details of edited Todo

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -3,16 +3,19 @@ import axios from "axios";
 
 // import { TodoContext } from './TodoContext';
 
+// UGH TARUNGON PA NI NGA PART!
+
 function Details({ clicked, click }) {
   const [details, setDetails] = useState("");
   const [putDetails, setPutDetails] = useState({});
+  const [edited, setEdited] = useState(false);
   const { id, title, date } = clicked;
 
   // Holds details of fetched Todo
   const [fetchedDetails, setFetchedDetails] = useState("");
   // State of Edit Button
   const [isToggled, setIsToggled] = useState(false);
-
+  const [willFetch, setWillFetch] = useState(true);
   const [editText, setEditText] = useState("");
 
   useEffect(() => {
@@ -21,6 +24,7 @@ function Details({ clicked, click }) {
   }, [editText]);
 
   useEffect(() => {
+    if(willFetch){
     console.log("in details use effect");
     axios
       .get(`http://localhost:8000/api/todo/${id}`, id, {
@@ -30,10 +34,11 @@ function Details({ clicked, click }) {
       })
       .then((res) => {
         let dat = res.data.data.details;
-        if (dat === undefined) {
-          dat = "way sud";
-        }
+        // if (dat === undefined) {
+        //   dat = "way sud";
+        // }
         setFetchedDetails(dat);
+        setEdited(!edited);
         console.log(dat);
       })
       .catch((err) => {
@@ -41,27 +46,35 @@ function Details({ clicked, click }) {
         console.log(err.message);
       });
     console.log("called!");
-  }, [id]);
+    console.log(willFetch);
+  }
+  }, [id, willFetch]);
 
   const handleChange = (e) => {
     console.log(editText);
     setEditText(e.target.value);
   };
+  // const handleChange = (e) =>{
+  //   setEditText({[e.target.name] : e.target.value});
+  //   console.log(editText);
+  // }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
+    // setWillFetch(!willFetch);
+    console.log(putDetails);
     console.log(editText);
-
     console.log(id, title, date, editText);
-    // console.log(details);
     setPutDetails({
       details: editText,
-    });
+    });   
+
     // console.log(details);
     console.log(putDetails);
     //
     axios
-      .put(`http://localhost:8000/api/todo/${id}`, putDetails)
+      .put(`http://localhost:8000/api/todo/${id}`, {details: editText})
       .then((req, res) => {
         console.log("details added!");
         console.log(req);
@@ -70,7 +83,10 @@ function Details({ clicked, click }) {
         console.log("Can't add details");
         console.log(err.message);
       });
+    console.log(willFetch);
     setIsToggled(!isToggled);
+    setWillFetch(!willFetch);
+    console.log(willFetch);
   };
   const editDetailText = (e) => {
     setEditText(e.target.value);
@@ -79,6 +95,7 @@ function Details({ clicked, click }) {
   const editing = () => {
     console.log("in editing!");
     setIsToggled(!isToggled);
+    setWillFetch(!willFetch);
     console.log(fetchedDetails);
     setEditText(fetchedDetails);
   };
@@ -92,10 +109,15 @@ function Details({ clicked, click }) {
           <div>Title: {title}</div>
           <span>Details</span>
           <button onClick={editing}>Edit</button>
-          <div>{fetchedDetails}</div>
+          {/* <div>{isToggled ? `${editText}` :fetchedDetails }</div>
+          {isToggled ? <div className:>{editText}</div>:} */}
+
+          {/* {isToggled ? <div className="hidden">{editText}</div> :<div>{fetchedDetails}</div>} */}
+          {<div>{editText}</div>}
+          {<div>{fetchedDetails}</div>}
         </>
       )}
-      {isToggled ? (
+      {isToggled &&
         <form id="details" onSubmit={handleSubmit}>
           <textarea
             name="details"
@@ -105,11 +127,11 @@ function Details({ clicked, click }) {
           ></textarea>
           <button>Save</button>
         </form>
-      ) : (
-        ""
-      )}
+      }
     </>
   );
 }
+
+// https://stackoverflow.com/questions/49718281/react-state-is-empty-upon-form-submission
 
 export default Details;
