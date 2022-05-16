e886781a63cd178a06f14a743d39b7914c98177c
himlayyy <alesandralelis@gmail.com>
2022-05-16 12:59:33 +0800

    Buttons for adding Board

--- client/src/components/Boards.jsx
+++ client/src/components/Boards.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import BoardsList from "./BoardsList";
 
 function Boards() {
+  const [toggleModal, setToggleModal] = useState(false);
+
+  const callModal = () => {
+    setToggleModal(true);
+    console.log("called modal");
+  };
+  const createBoard = () => {
+    setToggleModal(!toggleModal);
+  };
+
   return (
-    <div>Boards</div>
-  )
+    <>
+      <div className="boards">
+        Boards
+        <div className="container">
+          <BoardsList />
+          <button onClick={callModal}>+ Add Board</button>
+        </div>
+      </div>
+      {toggleModal && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <div className="modal-header">
+              <p>New board name</p>
+              <button onClick={createBoard}>Close</button>
+            </div>
+            <div className="board-form">
+              <form>
+                {/* <div> */}
+                  <input type="text" id="new-board" name="new-board" />
+                  <button onClick={createBoard}>Create Board</button>
+                {/* </div> */}
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Boards
+export default Boards;
