2cd8b6f1ceed63d0191c440b80d728a2b643ae84
himlayyy <alesandralelis@gmail.com>
2022-05-17 20:08:19 +0800

    Added components to display BoardList

--- client/src/components/Boards.jsx
+++ client/src/components/Boards.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import BoardsList from "./BoardsList";
+import axios from "axios";
 
 function Boards() {
   const [toggleModal, setToggleModal] = useState(false);
+  // const []
+  const [board, setBoard] = useState({board:""});
+  const [boardState, setBoardState] = useState("init");
 
   const callModal = () => {
     setToggleModal(true);
@@ -12,13 +16,43 @@ function Boards() {
   const createBoard = () => {
     setToggleModal(!toggleModal);
   };
+  
+  const handleChange = (e) =>{
+    setBoard({
+      ...board,
+      [e.target.name]: e.target.value
+    });
+    console.log(board);
+  }
 
-  return (
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setBoard({
+      ...board,
+      board: board,
+    });
+
+    axios
+    .post("http://localhost:8000/api/board",board)
+    .then((res) => {
+      console.log(board);
+      setBoard({
+        board:"",
+      });
+      setToggleModal(false);
+      setBoardState("updated");
+    })
+    .catch((err) => {
+      console.log("Error can't add board")
+    })    
+  }; 
+ 
+ return (
     <>
       <div className="boards">
         Boards
         <div className="container">
-          <BoardsList />
+          <BoardsList boardState={boardState} setBoardState={setBoardState}/>
           <button onClick={callModal}>+ Add Board</button>
         </div>
       </div>
@@ -30,10 +64,13 @@ function Boards() {
               <button onClick={createBoard}>Close</button>
             </div>
             <div className="board-form">
-              <form>
+              <form onSubmit={handleSubmit}>
                 {/* <div> */}
-                  <input type="text" id="new-board" name="new-board" />
-                  <button onClick={createBoard}>Create Board</button>
+                  <input type="text" 
+                  id="board" 
+                  name="board"
+                  onChange = {handleChange} />
+                  <button type="submit">Create Board</button>
                 {/* </div> */}
               </form>
             </div>
