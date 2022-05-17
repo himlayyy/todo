da124df411bb853e28d5514fd1430dcfa13d923f
himlayyy <alesandralelis@gmail.com>
2022-05-17 20:09:02 +0800

    BoardsList can fetch boards list

--- client/src/components/BoardsList.jsx
+++ client/src/components/BoardsList.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+import BoardItem from "./BoardItem";
 
-function BoardsList() {
+function BoardsList({ boardState, setBoardState }) {
+  const [boardList, setBoardList] = useState([]);
+
+  useEffect(() => {
+    switch (boardState) {
+      case "init":
+      case "updated":
+        axios
+          .get("http://localhost:8000/api/board")
+          .then((res) => {
+            setBoardList(res.data);
+            setBoardState("notUpdated");
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        break;
+      default:
+    }
+  }, [boardState]);
+
   return (
-    <div>BoardsList</div>
-  )
+    <>
+      <div>BoardsList</div>
+      <ul className="board-list">
+        {boardList.map((board) =>
+          <BoardItem 
+          key={board._Id}
+          board={board.board}
+          id={board.Id}
+        />
+        )}        
+      </ul>
+    </>
+  );
 }
 
-export default BoardsList
+export default BoardsList;
